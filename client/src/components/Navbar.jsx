1fd86f67dfe6ca01773610221255ca750e2272da
skyfoojs <skyfoojs@gmail.com>
2024-08-16 17:50:50 +0800

    Add Navbar code
    - commented import statement will be used

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+/* import { useState } from 'react';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai'; */
+
+const Navbar = () => {
+    return (
+        <nav className="w-full flex flex-row justify-evenly">
+            <div>Logo</div>
+            <div>Night Mode</div>
+            <div>Booking Details</div>
+        </nav>
+    );
+}
+
+export default Navbar;
