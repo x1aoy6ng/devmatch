d9875ec5b873e665ef75689c1ddf7aadaed24caf
skyfoojs <skyfoojs@gmail.com>
2024-08-16 19:57:13 +0800

    Create SortBar.jsx

---
+++ client/src/components/SortBar.jsx
@@ -0,0 +1,18 @@
+import { CiCirclePlus } from "react-icons/ci"; // Plus Button
+import { CiCircleMinus } from "react-icons/ci"; // Minus Button
+import { FaBed } from "react-icons/fa"; // Bed Icon
+import { FaWifi } from "react-icons/fa"; // Wi-Fi Icon
+import { IoIosArrowDown } from "react-icons/io"; // Arrow Down
+import { IoIosArrowUp } from "react-icons/io"; // Arrow Up
+
+const SortBar = () => {
+    return (
+        <div className="flex flex-row justify-evenly">
+            <div className="border-solid border-2 border-black rounded-lg w-52 h-3">
+                Hello
+            </div>
+        </div>
+    );
+}
+
+export default SortBar;
