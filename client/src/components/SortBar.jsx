e867a606e3f2a8eebc140e42b788ed3b0f788178
skyfoojs <skyfoojs@gmail.com>
2024-08-16 22:23:27 +0800

    Adjust size for hotel, state, sort by, search button and position of number of guest

--- client/src/components/SortBar.jsx
+++ client/src/components/SortBar.jsx
@@ -6,12 +6,69 @@ import { IoIosArrowDown } from "react-icons/io"; // Arrow Down
 import { IoIosArrowUp } from "react-icons/io"; // Arrow Up
 
 const SortBar = () => {
+    const inputStyles = "relative border-solid border-2 border-black rounded-lg w-24 h-10";
+    const buttonStyles = "border-solid border-2 border-black rounded-lg w-24 h-10 flex items-center justify-center mt-5"
+    const numOfGuestStyle = "flex flex-row gap-2 items-center my-2";
+
     return (
-        <div className="flex flex-row justify-evenly">
-            <div className="border-solid border-2 border-black rounded-lg w-52 h-3">
-                Hello
+        <div className="flex flex-col absolute top-20 left-10 gap-4 border-solid border-2 border-black/30 rounded-xl w-60 p-4">
+            <p>Price per night</p>
+
+            <div className="flex flex-row gap-4">
+                <div className="flex flex-col">
+                    <p>Min</p>
+                    <input type="text" className={inputStyles}/>
+                </div>
+                    
+
+                <div className="flex flex-col">
+                    <p>Min</p>
+                    <input type="text" className={inputStyles}/>
+                </div>
             </div>
-        </div>
+
+            <div>               
+                <p>Guest Rating</p>
+
+                <input type="radio" />
+                <label htmlFor="">  5</label>
+                <br/>
+                <input type="radio" />
+                <label htmlFor=""> &gt; 4</label>
+                <br/>
+                <input type="radio" />
+                <label htmlFor=""> &gt; 3</label>
+                <br/>
+            </div>
+
+            <div>
+                <div className={buttonStyles}>Hotel</div>
+
+                <div className={buttonStyles}>State</div>
+
+                <div className={buttonStyles}>Sort By</div>    
+            </div>
+
+            <div className="mt-2 flex flex-col items-center justify-center">
+                <div className={numOfGuestStyle}>
+                    <p>Adult</p>
+                    <CiCirclePlus className="ml-5" fontSize={30}/>
+                    <p className="text-lg">2</p>
+                    <CiCircleMinus fontSize={30}/>
+                </div>
+
+                <div className={numOfGuestStyle}>
+                    <p>Children</p>
+                    <CiCirclePlus fontSize={30}/>
+                    <p className="text-lg">2</p>
+                    <CiCircleMinus fontSize={30}/>
+                </div>
+            </div>
+
+            <div>
+                <div className="rounded-lg w-52 h-10 flex items-center justify-center mt-2 bg-blue-400 text-white font-bold">Search</div>
+            </div>
+    </div>
     );
 }
 
