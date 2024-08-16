d518fae27c3c93946ac8f5e380411a3d32f1574c
skyfoojs <skyfoojs@gmail.com>
2024-08-16 19:56:18 +0800

    Add icons and logo wording

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-/* import { useState } from 'react';
-import { HiMenuAlt4 } from 'react-icons/hi';
-import { AiOutlineClose } from 'react-icons/ai'; */
+import { useState } from 'react';
+import { MdOutlineDarkMode } from 'react-icons/md'; // Lightmode
+import { MdDarkMode } from "react-icons/md"; // Darkmode
+import { BsFillTicketDetailedFill } from "react-icons/bs"; // Booking Details
 
+const navButton = "mx-4 cursor-pointer"
 const Navbar = () => {
     return (
-        <nav className="w-full flex flex-row justify-evenly">
-            <div>Logo</div>
-            <div>Night Mode</div>
-            <div>Booking Details</div>
+        <nav className="w-full flex flex-row justify-evenly mt-4 relative">
+            <div className="absolute top-0 left-20 cursor-pointer">Logo</div>
+            <div className="flex flex-row absolute top-0 right-10">
+                <MdOutlineDarkMode fontSize={30} className={navButton}/>
+
+                <BsFillTicketDetailedFill fontSize={30} className={navButton}/>
+            </div>
         </nav>
     );
 }
