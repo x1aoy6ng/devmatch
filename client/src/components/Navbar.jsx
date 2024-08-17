1892a72e0e5183ee5db1d8bbf21eb386fe4a171e
skyfoojs <skyfoojs@gmail.com>
2024-08-17 15:57:31 +0800

    Change the Navbar to position fixed and z index 10

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { BsFillTicketDetailedFill } from "react-icons/bs"; // Booking Details
 import { FaWallet } from "react-icons/fa6"; // Wallet Icon
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import { SiHotelsdotcom } from "react-icons/si";
 
 const navButton = "cursor-pointer"
 const modeSize = 30;
@@ -24,12 +25,12 @@ const Navbar = () => {
     }*/
 
     return (
-        <nav className="w-full flex items-center justify-between py-3 px-8">
-            <div className="cursor-pointer text-3xl font-bold">Logo</div>
+        <nav className="w-full flex items-center justify-between py-5 px-8 fixed bg-white shadow-lg z-10">
+            <div className="cursor-pointer text-3xl font-bold ml-14 flex">SMTG <SiHotelsdotcom fontSize={30} className={`self-center ${navButton} mx-3`}/> </div>
             <div className="flex items-center">
                 <MdOutlineDarkMode 
                     fontSize={modeSize} 
-                    className={`self-center ${navButton} mr-6`} 
+                    className={`self-center ${navButton} mr-6`}     
                 />
                 <WalletSelector />
             </div>
