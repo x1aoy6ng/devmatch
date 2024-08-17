1ca0100db6ca5eb005be71b4a12aa2e8aca4a7f2
i4GS <admin@i4.gs>
2024-08-17 15:11:41 +0800

    Added Card and Partners in Home pages

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -1,11 +1,15 @@
+import Cards from "../Cards";
 import HeroSection from "../HeroSection";
 import Navbar from "../Navbar";
+import Partners from "../Partners";
 
 function Home (){
     return (
         <>
         <Navbar />
         <HeroSection />
+        <Cards />
+        <Partners />
         </>
     );
 }
