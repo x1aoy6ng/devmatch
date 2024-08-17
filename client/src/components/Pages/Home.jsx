b7156919e2579a7420a1840c451428adad58ec60
i4GS <admin@i4.gs>
2024-08-17 11:53:02 +0800

    Added Navbar

--- client/src/components/Pages/Home.jsx
+++ client/src/components/Pages/Home.jsx
@@ -1,8 +1,12 @@
 import HeroSection from "../HeroSection";
+import Navbar from "../Navbar";
 
 function Home (){
     return (
+        <>
+        <Navbar />
         <HeroSection />
+        </>
     );
 }
 
