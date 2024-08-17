1c3563de5d467ca4f7abe56d55fbdbf49831831a
skyfoojs <skyfoojs@gmail.com>
2024-08-17 14:47:27 +0800

    Create RoomImage component

---
+++ client/src/components/RoomImage.jsx
@@ -0,0 +1,13 @@
+const RoomImage = () => {
+    return(
+        <div className="w-full mt-16 flex justify-center items-center">
+            <div className="m-10 border-2 border-solid border-black w-5/6 h-96">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RoomImage
