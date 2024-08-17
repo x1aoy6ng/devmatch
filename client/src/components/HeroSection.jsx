4b6454ba750c70bf3cbf801bd75898a6cf812fd6
i4GS <admin@i4.gs>
2024-08-17 16:06:10 +0800

    Remove the icon beside learn more

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -8,12 +8,8 @@ function HeroSection() {
       <h1 className="text-white text-6xl md:text-7xl lg:text-8xl mb-4 font-bold">SMTG GOOD</h1>
       <p className="text-white text-lg md:text-2xl mt-2 font-bold">What are you waiting for?</p>
       <div className="flex flex-col md:flex-row mt-8 space-y-4 md:space-y-0 md:space-x-4">
-        <a href="/search" target="_blank" className="text-white font-bold py-3 px-8 border border-white rounded-full hover:bg-white hover:text-black transition duration-300">
-          GET STARTED
-        </a>
-        <button className="bg-blue-600 text-white font-bold py-3 px-8 rounded-full hover:bg-blue-500 transition duration-300 flex items-center justify-center">
-          LEARN MORE <i className="far fa-play-circle ml-2" />
-        </button>
+        <a href="/search" target="_blank" className="text-white font-bold py-3 px-8 border border-white rounded-full hover:bg-white hover:text-black transition duration-300">GET STARTED</a>
+        <button className="bg-blue-600 text-white font-bold py-3 px-8 rounded-full hover:bg-blue-500 transition duration-300 flex items-center justify-center">LEARN MORE</button>
       </div>
     </div>
   );
