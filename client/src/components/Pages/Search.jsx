1bd5b9b6058bd5a20939af50510211a79c983390
i4GS <admin@i4.gs>
2024-08-17 15:15:07 +0800

    Remove relative in css

--- client/src/components/Pages/Search.jsx
+++ client/src/components/Pages/Search.jsx
@@ -1,10 +1,10 @@
 import Navbar from '../Navbar'
-import RoomInfo from '../RoomInfo'
+import RoomInfo from '../RoomDetail'
 import SortBar from '../SortBar'
 
 const Search = () => {
     return (
-        <div className='relative w-full min-h-screen'>
+        <div className='w-full min-h-screen'>
           <Navbar/>
           <SortBar/>
           <RoomInfo/>
