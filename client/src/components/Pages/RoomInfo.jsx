c4ce95dd8aac776a13d1be43fd14995a6c976726
i4GS <admin@i4.gs>
2024-08-17 17:36:58 +0800

    Added footer into room info page

--- client/src/components/Pages/RoomInfo.jsx
+++ client/src/components/Pages/RoomInfo.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../Navbar'
 import RoomImage from '../RoomImage'
+import Footer from '../Footer';
 
 const RoomInfo = () => {
     return(
         <div className='w-full'>
             <Navbar/>
             <RoomImage/>
+            <Footer />
         </div>
     );
 }
