30a0c9ebee57f519d5d758620266200a6104c252
skyfoojs <skyfoojs@gmail.com>
2024-08-17 17:29:37 +0800

    Import all components needed and seperated a div for RoomImage

--- client/src/components/Pages/RoomInfo.jsx
+++ client/src/components/Pages/RoomInfo.jsx
@@ -1,11 +1,17 @@
 import Navbar from '../Navbar'
 import RoomImage from '../RoomImage'
+import BookingInfoButton from '../BookingInfoButton';
+import BookingDescription from '../RoomDescription';
 
 const RoomInfo = () => {
     return(
         <div className='w-full'>
             <Navbar/>
-            <RoomImage/>
+            <div className='pt-28'>
+                <RoomImage/>
+            </div>
+            <BookingInfoButton/>
+            <BookingDescription/>
         </div>
     );
 }
