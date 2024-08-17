bb785c9b8c3abc0ddcb99108b6d6076efb225599
i4GS <admin@i4.gs>
2024-08-17 21:12:14 +0800

    Added animation when scrolling

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import CardItem from './CardItem';
 
 function Cards() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+    });
+
+    AOS.refresh();
+  }, []);
+
   return (
     <div className='py-16 bg-white'>
-      <h1 className='text-4xl font-bold text-center mb-14 relative group'>
+      <h1
+        className='text-4xl font-bold text-center mb-14 relative group'
+        data-aos="fade-up"
+      >
         Explore Our Top Picks
         <span className="absolute left-1/2 transform -translate-x-1/2 bottom-[-20px] h-1 w-40 bg-blue-500 rounded-full transition-all duration-300 ease-in-out group-hover:w-64"></span>
       </h1>
@@ -14,36 +29,43 @@ function Cards() {
             src='/src/assets/img/jujutsukaisen.jpg'
             text='Jujutsu Kaishen Exhibition'
             label='Best seller'
+            path='/search'
+            animation='fade-up'
           />
           <CardItem
             src='/src/assets/img/fullmetal.jpg'
             text='Fullmetal Alchemist Exhibition'
             label='Staff Favorite'
             path='/'
+            animation='fade-up'
           />
           <CardItem
             src='/src/assets/img/onepiece.jpg'
             text='One Piece Asia Tour'
             label='Popular'
             path='/'
+            animation='fade-up'
           />
           <CardItem
             src='/src/assets/img/naruto.webp'
             text='Naruto Pop Up Store'
             label='Featured'
             path='/'
+            animation='fade-up'
           />
           <CardItem
             src='/src/assets/img/attack.jpeg'
             text='Attack On Titan: Final Exhibition'
             label='Popular'
             path='/'
+            animation='fade-up'
           />
           <CardItem
             src='/src/assets/img/chainsawman.jpg'
             text='Chainsaw Man Exhibition'
             label='New Arrival'
             path='/'
+            animation='fade-up'
           />
         </div>
       </div>
