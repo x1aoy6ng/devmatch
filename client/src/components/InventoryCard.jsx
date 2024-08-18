33d48457c14161ac7bbf400b35a4ec1b7e31cc0d
skyfoojs <skyfoojs@gmail.com>
2024-08-18 09:46:21 +0800

    Change styles

--- client/src/components/InventoryCard.jsx
+++ client/src/components/InventoryCard.jsx
@@ -5,11 +5,11 @@ const InventoryCard = () => {
     return (
         <div className="w-full flex flex-col justify-center items-center">
             <h1 className="font-bold text-3xl text-red-700">Your NFT Ticket</h1>
-            <div className="white-glassmorphism inv-card w-4/5 h-1/2 mt-10 mb-40 border-2 border-black border-solid p-10 rounded-lg">
-                <div className="w-64 flex flex-col border-2 border-black border-solid pb-24 rounded-md"> 
-                    <img className='rounded-t-md' src="https://i.scdn.co/image/ab67616d0000b27312fc853d36215f51c478e2d9" alt="" />
+            <div className="w-4/5 h-1/2 mt-10 mb-40 border-2 border-black border-solid p-10 rounded-lg">
+                <div className="white-glassmorphism inv-card w-64 flex flex-col border-2 border-black border-solid pb-24 rounded-md"> 
+                    <img className='rounded-t-md h-60' src="/src/assets/img/jujutsu-kaisen-exhibition-ticket.jpg" alt="" />
 
-                    <div>
+                    <div className="mt-3">
                         <p>Name:</p>
 
                         <p>Description:</p>
