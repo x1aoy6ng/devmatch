fd5ecc740a08c1135559a2fa81b4f7d3c142d3e4
i4GS <admin@i4.gs>
2024-08-18 09:10:35 +0800

    Change the look style and image of the banner, added arrow and forward icon for the banner slide

--- client/src/components/RoomImage.jsx
+++ client/src/components/RoomImage.jsx
@@ -1,25 +1,77 @@
-const RoomImage = () => {
-    return (
-        <div className="w-full flex justify-center items-center">
-            <div className="rounded-lg border-1 border-solid border-black w-5/6 h-96 relative overflow-hidden">
-                <div className="w-full h-full relative flex">
-                    <img className="w-1/2 h-full object-cover" src="/src/assets/img/jujutsukaisen.jpg" alt="pool.jpg" />
-                    
-                    <div className="absolute w-1/4 right-1/4 top-1/2 transform -translate-y-1/2 flex flex-col">
-                        <div className="flex flex-row">
-                            <img className="object-cover" src="https://d1ef7ke0x2i9g8.cloudfront.net/kuala-lumpur/_large700/3098018/A-Jujutsu-Kaisen-Exhibition-is-Coming-to-Kuala-Lumpur.webp" alt="" />
-                            <img className="object-cover" src="https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_862/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/lmouuuy6nrtilksa9xjg/AnimationJujutsuKaisenExhibitioninMalaysia.webp" alt="" />
-                        </div>
+import React from 'react';
+import { MdArrowBack, MdArrowForward } from 'react-icons/md';
 
-                        <div className="flex flex-row ">
-                            <img className="object-cover" src="https://lh5.googleusercontent.com/fi-v_5iMEuxNgyCHxfIxrB3Dqe-Lm1yXk-MwCMvKGnAG5k-vufVsJ8HDZDsmhbamXl5CXg6xfs4Xd4PatIw1AbhcNALZYUphJB2_JOba7DE7U2vXy4hEuB9tH9td_loC4TMFilaxIoqqaDumFZbMSA" alt="" />
-                            <img className="object-cover" src="https://preview.redd.it/gqne9eb553e71.png?auto=webp&s=7ed4cfa133b630be87e2155e4cce65a13398916b" alt="" />
+const images = [
+    "/src/assets/img/jujutsukaisen.jpg",
+    "/src/assets/img/banner2.png",
+    "/src/assets/img/banner3.png",
+    "/src/assets/img/banner4.png",
+    "/src/assets/img/banner5.webp"
+];
+
+const RoomImage = () => {
+    const [currentIndex, setCurrentIndex] = React.useState(0);
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            handleNext();
+        }, 3000);
+        return () => clearInterval(interval);
+    }, [currentIndex]);
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    };
+
+    return (
+        <div className="relative w-full flex justify-center items-center mt-5">
+            <div className="relative rounded-2xl w-4/5 md:w-3/4 lg:w-1/2 h-96 overflow-hidden bg-white shadow-xl">
+                <div className="relative w-full h-full flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                    {images.map((img, index) => (
+                        <div
+                            key={index}
+                            className="w-full h-full flex-shrink-0"
+                        >
+                            <img
+                                className="w-full h-full object-cover"
+                                src={img}
+                                alt={`Slide ${index}`}
+                            />
                         </div>
-                    </div>
+                    ))}
+                </div>
+
+                {/* Navigation Arrows with Glass Effect */}
+                <button
+                    onClick={handlePrev}
+                    className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/50 backdrop-blur-sm border border-gray-300 text-gray-800 p-2 rounded-full shadow-md hover:bg-white/70 flex items-center justify-center"
+                >
+                    <MdArrowBack size={24} />
+                </button>
+                <button
+                    onClick={handleNext}
+                    className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/50 backdrop-blur-sm border border-gray-300 text-gray-800 p-2 rounded-full shadow-md hover:bg-white/70 flex items-center justify-center"
+                >
+                    <MdArrowForward size={24} />
+                </button>
+
+                {/* Indicators */}
+                <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+                    {images.map((_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => setCurrentIndex(index)}
+                            className={`w-3 h-3 rounded-full ${index === currentIndex ? 'bg-gray-800' : 'bg-gray-300'} focus:outline-none`}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default RoomImage;
